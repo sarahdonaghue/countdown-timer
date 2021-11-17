039539866c9fae0eaaafc03082ce8a46c6f2116f
Sarah Donaghue <sarahmdonaghue@gmail.com>
2021-11-16 19:57:55 -0500

    zeroGravity

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ const hoursEl = document.getElementById("hours");
 const minsEl = document.getElementById("mins");
 const secondsEl = document.getElementById("seconds");
 
-const newYears = "1 Jan 2021";
+const zeroGravity = "31 Dec 2021";
 
 function countdown() {
-    const newYearsDate = new Date(newYears);
+    const zeroGravityDate = new Date(zeroGravity);
     const currentDate = new Date();
 
-    const totalSeconds = (newYearsDate - currentDate) / 1000;
+    const totalSeconds = (zeroGravityDate - currentDate) / 1000;
 
     const days = Math.floor(totalSeconds / 3600 / 24);
     const hours = Math.floor(totalSeconds / 3600) % 24;
